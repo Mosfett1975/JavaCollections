16f38e3b06ba61fdce98b4dfeaaed6b37510f078
Dmitry Maltsev <dmitry.maltsev@bsc-idea.com>
2020-04-06 17:06:52 +0300

    no message

--- src/main/java/org/epam/javaCollection/RunStream.java
+++ src/main/java/org/epam/javaCollection/RunStream.java
@@ -1,22 +1,32 @@
 package org.epam.javaCollection;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-
+import static javafx.scene.input.KeyCode.T;
 
 public class RunStream {
     static int sizeOfArray = 1000000;
 
-
     public static void main(String[] args) {
 
-
 /** Create list **/
-        List<Integer> list = new ArrayList<>();
+      List<Integer> list = new ArrayList<>();
+
 
 
 /** Fill list with sequence 0 - 1000000 **/
@@ -25,11 +35,16 @@ public class RunStream {
 /** shuffle the list **/
         Collections.shuffle(list);
 
-/** Check for uniqueness of elements -
- Set always contains only unique keys **/
-        HashSet h = new HashSet();
-        list.stream().forEach(h::add);
-        if (list.size() == h.size()) System.out.println("List contains only unique keys!");
+/** Get minimal value in list **/
+System.out.println(list.stream().min(Integer::compare).get() + " - This is minimal value");
+
+        System.out.println(list.stream().distinct().collect(Collectors.toList()));
+
+///** Check for uniqueness of elements -
+// Set always contains only unique keys **/
+//        HashSet h = new HashSet();
+//        list.stream().forEach(h::add);
+//        if (list.size() == h.size()) System.out.println("List contains only unique keys!");
 
 
 /** Remove odd elemets from list. Only tenth part of array**/
@@ -37,8 +52,17 @@ public class RunStream {
         List firstTen = list.subList(0, 10);
         System.out.println("Display that the list has even numbers only  - " + firstTen);
 
+        List<Integer> distinctElements = list.stream().filter(distinctByKey(p -> p.getClass())).collect(Collectors.toList());
+
+        System.out.println(distinctElements + "fdfd");
+
+        System.out.println(list.stream().filter(distinctByKey(list::add)) + "dsfsdfs");
 
     }
 
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
+    }
 
 }
